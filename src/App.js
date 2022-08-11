6a2037f02745643bc281bb4196bde28e8c18fced
ifzyy <johnsonnifemi8@gmail.com>
2022-08-11 21:46:20 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
+import Nav from './components/navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/book.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function book() {
+  return (
+    <div>
+      <div className= "container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default book
