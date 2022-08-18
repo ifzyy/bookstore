dcc8e3b6f2859a5eb79f7200725afee3f0426c9b
ifzyy <johnsonnifemi8@gmail.com>
2022-08-18 17:51:45 +0100

    fixed linters error

--- src/redux/Book/Book.js
+++ src/redux/Book/Book.js
@@ -6,25 +6,25 @@ const initialState = [];
 
 // Reducer
 const bookReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_BOOK:
-            return [...state, action.book];
-        case REMOVE_BOOK:
-            return state.filter((book) => book.id !== action.id);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
 };
 
 // Action Creator
 export const addBook = (book) => ({
-    type: ADD_BOOK,
-    book,
+  type: ADD_BOOK,
+  book,
 });
 
 export const removeBook = (id) => ({
-    type: REMOVE_BOOK,
-    id,
+  type: REMOVE_BOOK,
+  id,
 });
 
 export default bookReducer;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,10 +3,10 @@ import bookReducer from './Book/Book';
 import categoriesReducer from './Categories/Categories';
 
 const store = configureStore({
-    rootReducer: {
-        books: bookReducer,
-        categories: categoriesReducer,
-    },
+  rootReducer: {
+    books: bookReducer,
+    categories: categoriesReducer,
+  },
 });
 
 export default store;
